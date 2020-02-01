ad881f77579b9c54bd87638c54fba0cade501d8b
Eugen <shevchenko2786@gmail.com>
2020-02-02 00:19:34 +0200

    change data

---
+++ js/ajax.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(".send").click(
+        function(){
+            sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+            return false;
+        }
+    );
+});
+
+function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+        url:     url,
+        type:     "POST",
+        dataType: "html",
+        data: $("#"+ajax_form).serialize(),
+        success: function(response) {
+            result = $.parseJSON(response);
+            $('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
+        },
+        error: function(response) { // Данные не отправлены
+            $('#result_form').html('Ошибка. Данные не отправлены.');
+        }
+    });
+}
