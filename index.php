804466367461acf045f79194b4a4081db26bcc25
Eugen <shevchenko2786@gmail.com>
2020-02-02 00:20:59 +0200

    change data

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSS -->
+    <!-- CSS Fuck-->
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
